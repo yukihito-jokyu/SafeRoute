950753a7259e7d75feee40e614e60109b54a5d63
yukihito-jokyu <119581059+yukihito-jokyu@users.noreply.github.com>
2025-04-24 15:18:41 +0900

    [add]storybookレスポンシブ機能の追加

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -9,7 +9,8 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@chromatic-com/storybook",
-    "@storybook/experimental-addon-test"
+    "@storybook/experimental-addon-test",
+    '@storybook/addon-viewport'
   ],
   "framework": {
     "name": "@storybook/react-vite",
